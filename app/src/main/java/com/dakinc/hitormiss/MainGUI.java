92afc0a752b14cf5dd74ee134cf6385616fd7d68
DanielTran3 <dtran3@ualberta.ca>
2017-01-28 13:23:04 -0700

    New Project

---
+++ app/src/main/java/com/dakinc/hitormiss/MainGUI.java
@@ -0,0 +1,13 @@
+package com.dakinc.hitormiss;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainGUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_gui);
+    }
+}
